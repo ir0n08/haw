a94fa31487088eca640927481ee7bb95f8625ead
ir0n08 <paul.eisenmann@outlook.com>
2020-06-08 16:35:02 +0200

    Sem2 Task3 fix: Missing functions for MagicSquare.java

--- year01-02/Task3/MagicSquare.java
+++ year01-02/Task3/MagicSquare.java
@@ -7,15 +7,14 @@ package com.prosiebensat1tech.haw_praktikum_3;
 * @author  Eisenmann, Paul
 * @see paul.eisenmann@prosiebensat1.com
 * @see https://github.com/ir0n08/haw/tree/master/year01-02/Task3
-* @version 1.0
-* @since   05-06-2020 
+* @version 1.1 fixed version: added missing vertical/horizontal checksum
+* @since   08-06-2020 
 */
 
 public class MagicSquare {
 
 	public static int[][] getExample() {
-		int[][] example = {{8,1,6},{3,5,7},{4,9,2}}; // create example array of magic square
-		return example; // return the example ###! QUESTION !### Is it possible to return a multi dim array in one line? Thank you!
+		return new int[][] {{8,1,6},{3,5,7},{4,9,2}}; // return the example # Thank you!
 	}
 	
 	public static boolean isMagic(int[][] magicArray) { // input is a 2 dim array
@@ -35,15 +34,25 @@ public class MagicSquare {
 				throw new IllegalArgumentException("error: your square is not a square");
 			}
 						
-			int currentSum = 0; // create a var for the current sum in check
+			int currentHorizontalSum = 0; // create a var for the current sum in check -> Horizontal Line
 			for(int j = 0; j < magicArray[i].length; j++) {
-				currentSum += magicArray[i][j]; // add every entry
+				currentHorizontalSum += magicArray[i][j]; // add every entry
 			}
 			
-			if(i != 0 && currentSum != lastSum) { // if its not the 1st run trough, check if the last sum is identical with the current sum
+			int currentVerticalSum = 0; // create a var for the current sum in check -> Vertical Line
+			for(int k = 0; k < magicArray[i].length; k++) {
+				currentVerticalSum += magicArray[k][i]; // add every entry
+			}
+			
+			int currentDiagonalSum = 0; // create a var for the current sum in check -> Diagonal Line
+			for(int l = 0; l < magicArray[i].length; l++) {
+				currentDiagonalSum += magicArray[l][l]; // add every entry
+			}
+			
+			if(i != 0 && (currentHorizontalSum != lastSum || currentVerticalSum != lastSum || currentDiagonalSum != lastSum)) { // if its not the 1st run trough, check if the last sum is identical with the current sum
 				return false; // if thats not the case, return false
 			}
-			lastSum = currentSum; // after the check, you assign the current sum as the last sum for the next loop
+			lastSum = currentHorizontalSum; // after the check, you assign the current sum as the last sum for the next loop
 		}
 		return true; // if none of the above return false have fired, you get here and your square is there for magical :)
 	}
